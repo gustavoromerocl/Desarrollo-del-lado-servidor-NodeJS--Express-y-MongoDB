99d68f3267e0a619f36d0d88c27681515bf9c777
romerodevelopment <64169519+romerodevelopment@users.noreply.github.com>
2021-03-17 23:04:49 -0300

    Creando un proyecto con node - MVC

---
+++ Creando un proyecto Node/red_bicicletas/models/bicicleta.js
@@ -0,0 +1,43 @@
+var Bicicleta = function(id, color, modelo, ubicacion){
+    this.id = id;
+    this.color = color;
+    this.modelo = modelo;
+    this.ubicacion = ubicacion;
+}
+
+Bicicleta.prototype.toString = function(){
+    return 'id ' + this.id + ' | color: ' + this.color; 
+}
+
+Bicicleta.allBicis = [];
+
+Bicicleta.add = function(aBici){
+    Bicicleta.allBicis.push(aBici)
+};
+
+Bicicleta.findById = function(aBiciId){
+    var aBici = Bicicleta.allBicis.find(x => x.id ==     aBiciId);
+    if(aBici)
+        return aBici;
+    else
+        throw new Error(`No existe un bici con el id que indicas ${aBiciId}`)
+}
+
+Bicicleta.removeById = function(aBiciId){
+    console.log(aBiciId);
+    let aBici = Bicicleta.findById(aBiciId);
+    if(aBici) {
+        const newAllbicis =  Bicicleta.allBicis.filter(x => x.id != aBiciId);
+        Bicicleta.allBicis = newAllbicis;
+    }
+    else
+        throw new Error(`No existe una bici con el id ${aBiciId}`)
+}
+
+var a = new Bicicleta(1, 'rojo', 'urbana', []);
+var b = new Bicicleta(2, 'blanca', 'urbana', []);
+
+Bicicleta.add(a);
+Bicicleta.add(b);
+
+module.exports = Bicicleta;
